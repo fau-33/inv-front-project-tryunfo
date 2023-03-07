31874aedd9adc9e0f7dffa49ec97466d63dc7a90
Flávio Félix <fau_felix30@hotmail.com>
2023-03-07 19:59:12 -0300

    Feito o req 5

--- src/App.js
+++ src/App.js
@@ -3,35 +3,78 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardImage: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+    isSaveButtonDisabled: true,
+    cards: [],
+  };
 
-    this.onInputChange = this.onInputChange.bind(this);
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, () => {
+      this.validationButtonSubmit();
+    });
+  };
 
-    this.state = {
+  validationButtonSubmit = () => {
+    const { cardName, cardDescription, cardImage, cardAttr1, cardAttr2,
+      cardAttr3 } = this.state;
+    let buttonIsDisabled = true;
+    const cardLimit = 91;
+    const totalLimit = 211;
+
+    const sum = parseFloat(cardAttr1)
+    + parseFloat(cardAttr2) + parseFloat(cardAttr3) < totalLimit;
+    const min = cardName.length > 0 && cardDescription.length > 0 && cardImage.length > 0;
+    const max = cardAttr1 < cardLimit && cardAttr2 < cardLimit && cardAttr3 < cardLimit;
+    const negative = cardAttr1 >= 0 && cardAttr2 >= 0 && cardAttr3 >= 0;
+
+    if (sum && min && max && negative) {
+      buttonIsDisabled = false;
+    }
+
+    this.setState({ isSaveButtonDisabled: buttonIsDisabled });
+  };
+
+  onSaveButtonClick = () => {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo } = this.state;
+    const card = {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    };
+
+    this.setState((previState) => ({
+      cards: [...previState.cards, card],
       cardName: '',
       cardDescription: '',
       cardImage: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
       cardRare: 'normal',
       cardTrunfo: false,
-
-    };
-  }
-
-  onInputChange({ target }) {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    });
-  }
+      isSaveButtonDisabled: true,
+    }));
+  };
 
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo } = this.state;
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled } = this.state;
 
     return (
       <div>
@@ -45,7 +88,9 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
           cardName={ cardName }
@@ -56,11 +101,10 @@ class App extends React.Component {
           cardAttr3={ cardAttr3 }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
-
         />
-
       </div>
     );
   }
 }
+
 export default App;
